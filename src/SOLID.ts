ff5d44455929b0c10dab4af18ac86cd4697724ed
thedarkking01 <sourabhsinghbais52@gmail.com>
2026-05-20 23:50:08 +0530

    learning solid by 's'

---
+++ src/SOLID.ts
@@ -0,0 +1,72 @@
+// Single Responsibility Principle (SRP) states:
+
+// “A class should have only one reason to change.”
+
+// This means:
+
+// A class should do only one job
+// It should have only one responsibility
+
+// If a class handles multiple tasks, changes in one task may affect the others, making the code harder to maintain.
+
+class User {
+  constructor(
+    public name: string,
+    public email: string
+  ) {}
+}
+
+class UserValidator {
+
+  validate(user: User): boolean {
+
+    if (!user.email.includes("@")) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+class UserRepository {
+
+  save(user: User): void {
+    console.log("User saved to database");
+  }
+}
+class EmailService {
+
+  sendWelcomeEmail(user: User): void {
+    console.log(`Welcome email sent to ${user.email}`);
+  }
+}
+
+class UserService {
+
+  constructor(
+    private validator: UserValidator,
+    private repository: UserRepository,
+    private emailService: EmailService
+  ) {}
+
+  createUser(name: string, email: string): void {
+
+    // Create user object
+    const user = new User(name, email);
+
+    // Validate user
+    const isValid = this.validator.validate(user);
+
+    if (!isValid) {
+      throw new Error("Invalid user data");
+    }
+
+    // Save user
+    this.repository.save(user);
+
+    // Send email
+    this.emailService.sendWelcomeEmail(user);
+
+    console.log("User registration completed");
+  }
+}
